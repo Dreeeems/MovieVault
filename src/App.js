22196a7dcbe86cb2e857a27beab4eab6a3c7f99e
Dreeeems <114916033+Dreeeems@users.noreply.github.com>
2024-07-24 14:38:42 +0200

    adding router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Home from "./pages/home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App dark:bg-gray-800 h-[100vh] overflow-y-hidden">
-      <Home />
+      <Router>
+        <Routes>
+          <Route path="/" Component={Home}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
